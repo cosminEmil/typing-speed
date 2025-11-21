d2512b05c2f527fe42a713376020bfd5f358dc3d
Cosmin Emil Bucur <cosminbucur2312@gmail.com>
2025-11-21 22:22:40 +0200

    added three more sentences

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let words1 = [ "car ", "table ", "water ", "book ", "door ", "apple ", "street ", "light ", "morning ", "river ", "garden" ];
 let words2 = ["bread ", "road ", "family ", "flower ", "morning ", "river ", "garden ","note ", "hand ", "wall "];
 let words3 = ["sun ", "moon ", "bag ", "train ", "voice ","note ", "hand ", "wall ", "floor ", "car ", "table ", "water "];
+let words4 = ["tree ", "cloud ", "river ", "stone ", "paper ", "glass ", "light ", "field ", "road ", "chair ", "window ", "door "];
+let words5 = ["house ", "ship ", "bird ", "flower ", "shadow ", "path ", "bridge ", "rain ", "wind ", "book ", "star ", "leaf "];
+let words6 = ["mountain ", "forest ", "storm ", "fire ", "snow ", "sand ", "coffee ", "music ", "letter ", "street ", "camera ", "clock "];
 
 let firstSentence = document.getElementById('letters1');
 let secondSentence = document.getElementById('letters2');
 let thirdSentence = document.getElementById('letters3');
+let fourthSentence = document.getElementById('letters4');
+let fifthSentence = document.getElementById('letters5');
+let sixthSentence = document.getElementById('letters6');
+
 let cnt = 0;
 
 function generateSentence(sentence, id) {
@@ -18,6 +25,12 @@ function generateSentence(sentence, id) {
                 secondSentence.appendChild(letter);
             } else if (id == 3) {
                 thirdSentence.appendChild(letter);
+            } else if (id == 4) {
+                fourthSentence.appendChild(letter);
+            } else if (id == 5) {
+                fifthSentence.appendChild(letter);
+            } else if (id == 6) {
+                sixthSentence.appendChild(letter);
             }
         }
     }
@@ -41,9 +54,14 @@ function start() {
     generateSentence(words1, 1);
     generateSentence(words2, 2);
     generateSentence(words3, 3);
+    generateSentence(words4, 4);
+    generateSentence(words5, 5);
+    generateSentence(words6, 6);
 }
 
-let firstIndex = 0, secondIndex = 0, thirdIndex = 0, isCorrect = 1, wordCounter = 0;
+let firstIndex = 0, secondIndex = 0, thirdIndex = 0;
+let fourthIndex = 0, fifthIndex = 0, sixthIndex = 0;
+let isCorrect = 1, wordCounter = 0;
 
 function checkLetter(typedLetter, inTextLetter, index, sentence, sentenceLength) {
     if (inTextLetter != ' ') {
@@ -73,9 +91,18 @@ function getInput() {
         let secondInTextLetter = secondSentence.childNodes[secondIndex].innerText;
         secondIndex = checkLetter(typedLetter, secondInTextLetter, secondIndex, secondSentence.childNodes, secondSentence.childNodes.length);
     } else if (thirdIndex < thirdSentence.childNodes.length) {
-        thirdInTextLetter = thirdSentence.childNodes[thirdIndex].innerText;
+        let thirdInTextLetter = thirdSentence.childNodes[thirdIndex].innerText;
         thirdIndex = checkLetter(typedLetter, thirdInTextLetter, thirdIndex, thirdSentence.childNodes, thirdSentence.childNodes.length);
-    } else  {
+    } else  if (fourthIndex < fourthSentence.childNodes.length) {
+        let fourthInTextLetter = fourthSentence.childNodes[fourthIndex].innerText;
+        fourthIndex = checkLetter(typedLetter, fourthInTextLetter, fourthIndex, fourthSentence.childNodes, fourthSentence.childNodes.length);
+    } else  if (fifthIndex < fifthSentence.childNodes.length) {
+        let fifthInTextLetter = fifthSentence.childNodes[fifthIndex].innerText;
+        fifthIndex = checkLetter(typedLetter, fifthInTextLetter, fifthIndex, fifthSentence.childNodes, fifthSentence.childNodes.length);
+    } else  if (sixthIndex < sixthSentence.childNodes.length) {
+        let sixthInTextLetter = sixthSentence.childNodes[sixthIndex].innerText;
+        sixthIndex = checkLetter(typedLetter, sixthInTextLetter, sixthIndex, sixthSentence.childNodes, sixthSentence.childNodes.length);
+    } else {
         let message = document.getElementById('message');
         message.innerText = "SCORE:" + wordCounter;
         setTimeout(() => {
